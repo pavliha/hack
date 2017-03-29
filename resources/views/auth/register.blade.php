500343aa7b2a637b29598d21809081c7a029fb90
Alexander <snosme@gmail.com>
2017-03-29 22:01:35 +0300

    restyle register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 offset-md-2">
+        <div class="col-md-6 offset-md-3">
             <div class="card">
                 <div class="card-header">Register</div>
                 <div class="card-block">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-2 col-form-label">Name</label>
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }} row">
+                            <label for="name" class="col-sm-4 col-form-label">Name</label>
 
-                            <div class="col-md-6">
+                            <div class="col-sm-8">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -24,10 +24,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-2 col-form-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} row">
+                            <label for="email" class="col-sm-4 col-form-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class="col-sm-8">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -38,28 +38,38 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">
-                            <label for="role" class="col-2 col-form-label">Role</label>
+                        <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }} row">
+                            <label for="role" class="col-sm-4 col-form-label">Role</label>
 
-                            <div class="col-md-6">
-                                <label class="radio-inline"><input type="radio" value="admin" name="role">Admin</label>
-                                <label class="radio-inline"><input type="radio" value="teamlead" name="role">Team-lead</label>
-                                <label class="radio-inline"><input type="radio" value="perfomer" name="role">Perfomer</label>
+                            <div class="col-sm-8">
+                                <label class="custom-control custom-radio">
+                                  <input name="radio" type="radio" class="custom-control-input" value="admin" name="role">
+                                  <span class="custom-control-indicator"></span>
+                                  <span class="custom-control-description">Admin</span>
+                                </label>
+                                <label class="custom-control custom-radio">
+                                  <input name="radio" type="radio" class="custom-control-input" value="teamlead" name="role">
+                                  <span class="custom-control-indicator"></span>
+                                  <span class="custom-control-description">Team-lead</span>
+                                </label>
+                                <label class="custom-control custom-radio">
+                                  <input name="radio" type="radio" class="custom-control-input" value="perfomer" name="role" checked>
+                                  <span class="custom-control-indicator"></span>
+                                  <span class="custom-control-description">Perfomer</span>
+                                </label>
 
                                 @if ($errors->has('role'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('role') }}</strong>
                                     </span>
                                 @endif
-
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} row">
+                            <label for="password" class="col-sm-4 col-form-label">Password</label>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-2 col-form-label">Password</label>
-
-                            <div class="col-md-6">
+                            <div class="col-sm-8">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -70,17 +80,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-2 col-form-label">Confirm Password</label>
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-sm-4 col-form-label">Confirm Password</label>
 
-                            <div class="col-md-6">
+                            <div class="col-sm-8">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group row">
+                            <div class="col-sm-8 offset-sm-4">
+                                <button type="submit" class="btn btn-primary btn-success">
                                     Register
                                 </button>
                             </div>
