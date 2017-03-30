1e8c6551b6e68beed3c51d3d1b546b3ab0c11c45
Alexander <snosme@gmail.com>
2017-03-30 17:48:12 +0300

    done

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="card-block">
-                    <div v-for="(task, index) in tasks" class="task" :class="{ opened: active === index }" v-if="isTaskVisible(index)">
+                    <div v-for="(task, index) in tasks" class="task" :class="{ opened: active === index, done: task.completed }" v-if="isTaskVisible(index)">
                         <div class="row no-gutters">
                             <label class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" v-model="task.completed" @click="completeTask(index)">
@@ -33,34 +33,40 @@
                         </div>
                         <div class="col-sm-12 task-body task-footer">
                             <span>@{{ task.updated_at }}</span>
-                            <button class="action" data-toggle="modal" data-target="#exampleModal">@{{ assignedInfo(index) }}</button>
-                            <button class="action" @click="editing = !editing">Edit task</button>
+                            <button class="action" data-toggle="modal" data-target="#assignModal">@{{ assignedInfo(index) }}</button>
+                            <button class="action" @click="editing = !editing">Edit task <span v-if="editing">(hit Enter to save)</span></button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="exampleModal">
+    <div class="modal fade" id="assignModal">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Assigned users</h5>
           </div>
           <div class="modal-body" v-if="active != -1">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search users (пока-что бесполезное говно)" autofocus>
-            </div>
-            <ul class="list-group">
-              <li v-for="user in tasks[active].users" class="list-group-item justify-content-between">
+            <ul class="list-group" @click="searchingUser = false">
+              <li v-for="(user, userIdx) in tasks[active].users" class="list-group-item justify-content-between">
                 @{{ user.name }}
-                <span class="btn btn-outline-danger btn-sm">Delete</span>
+                <button class="btn btn-outline-danger btn-sm" @click="tasks[active].users.splice(userIdx, 1)">Delete</button>
               </li>
             </ul>
+            <div class="form-group users-ac-wrapper">
+                <input v-model="userQuery" type="text" class="form-control" placeholder="Search users"
+                    @focus="searchingUser = true">
+                <div class="list-group users-ac" v-if="searchingUser">
+                  <button v-for="user in matchedUsers" type="button" class="list-group-item list-group-item-action"
+                    :disabled="isUserAssigned(user.id, active)"
+                    @click="tasks[active].users.push(user)">@{{ user.name }}</button>
+                </div>
+            </div>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary">Save</button>
+            <button type="button" class="btn btn-primary" @click="saveAssigments()" data-dismiss="modal">Save</button>
           </div>
         </div>
       </div>
@@ -76,19 +82,32 @@
         el: '#task-app',
         data: {
             tasks: [],
+            users: [],
+            userQuery: '',
+            searchingUser: false,
             active: -1,
             editing: false,
-            showCompleted: false,
-            showAll: false
-            // true - задачи назначиные лично вам
-            // false - все задачи
-            //   - для директора: позволяет редактировать и отмечать "готово" не назначеные для него задачи 
-            //   - для тимлида:   позволяет редактировать, назначать и отмечать "готово" не назначеные для него задачи 
+            showCompleted: false, // показать "готовые" задачи
+            showAll: false // показать задачи не назначеные вам
         },
         created: function () {
             axios.get('/api/tasks').then(response => {
                 this.tasks = response.data;
             });
+            axios.get('/api/users').then(response => {
+                this.users = response.data;
+            });
+        },
+        computed: {
+            matchedUsers: function () {
+                var ret = [];
+                var self = this;
+                this.users.forEach(function (user) {
+                    if (user.name.indexOf(self.userQuery) != -1)
+                        ret.push(user);
+                });
+                return ret;
+            }
         },
         methods: {
             completeTask: function (idx) {
@@ -143,6 +162,13 @@
                     name: task.name
                 });
                 this.editing = false;
+            },
+            saveAssigments: function () {
+                var task = this.tasks[this.active];
+                axios.put('/api/tasks/' + task.id, {
+                    id: task.id,
+                    users: task.users
+                });
             }
         }
     })
